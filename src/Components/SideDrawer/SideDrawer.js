467a68f9f351887cdb41f2c77950985436123daf
Jashan Chahal <chahaljashan123@gmail.com>
2019-12-31 17:15:05 +0530

    Added icon

--- src/Components/SideDrawer/SideDrawer.js
+++ src/Components/SideDrawer/SideDrawer.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './SideDrawer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAcquisitionsIncorporated } from '@fortawesome/free-brands-svg-icons'
 const sideDrawer = props => {
     let drawerClasses = 'side-drawer'
     if(props.show){
@@ -7,7 +9,7 @@ const sideDrawer = props => {
     }
     return (
     <nav className={drawerClasses}>
-        <span className="navbar-brand"><strong>Alumni Network</strong></span>
+        <span className="navbar-brand"><FontAwesomeIcon icon={faAcquisitionsIncorporated} size="2x"></FontAwesomeIcon></span>
         <ul>    
             <li><a href="/">Home</a></li>
             <li><a href="/">Profile</a></li>
