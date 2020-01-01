d9d5fee965cb16d2854f6b836e629c1233216cef
Nakkina Bhargav <43701429+bhargav462@users.noreply.github.com>
2020-01-01 12:55:38 +0530

    adding server

---
+++ Server/server.js
@@ -0,0 +1,51 @@
+var config = require('./config/config');
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
+const hbs = require('hbs');
+
+const {user} = require('./models/user');
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/Alumini");
+
+const app = express();
+app.use(bodyParser.json());
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/views'));
+
+
+app.get('/',(req,res) => {
+  res.render('index.hbs');
+});
+
+app.post('/add',(req,res) => {
+   var body = _.pick(req.body,['userName','password']);
+   var newUser = new user(body);
+
+   newUser.save().then(() => {
+       console.log('Check database');
+       res.send(newUser);
+   },(e) => {
+       res.sendStatus(400).send(e);
+   });
+});
+
+app.post('/check',(req,res) => {
+    
+     user.findOne({userName:req.body.userName,password:req.body.password}).then((user1) => {
+       if(!user1)
+       {
+           res.sendStatus(404).send("No user found");
+       }else{
+           res.send(user1);
+       }
+     });
+
+});
+
+app.listen(3002,() => {
+    console.log(`Server is up on port ${3002}`);
+});
